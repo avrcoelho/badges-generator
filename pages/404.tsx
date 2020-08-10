a69b04a94afd2f4d6c5f1b10c609a3c69207d8ed
André Coelho <andrevrcoelho@MacBook-Pro-de-Andre.local>
2020-08-10 12:14:32 -0300

    feat: back button in 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -13,7 +13,7 @@ const Page404: FC = () => {
         <Content404>
           <h1>404</h1>
           <p>This page could not be found</p>
-          <a href="/">Home page</a>
+          <a href="/">Go to home</a>
         </Content404>
       </Main>
       <Footer />
